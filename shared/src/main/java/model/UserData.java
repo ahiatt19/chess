b37aedc2b3f669c5547adbd8d3138577db1a084a
Aubry <ahiatt19@byu.edu>
2024-10-19 10:47:05 -0600

    The Register Handler is working now

--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -1,3 +1,7 @@
 package model;
 
-public record UserData(String username, String password, String email) {}
+public record UserData(String username, String password, String email) {
+    public boolean isUserNameEqual(String otherUsername) {
+        return username.equals(otherUsername);
+    }
+}
