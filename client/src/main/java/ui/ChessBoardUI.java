8a7187db3d7ad3427d5760c1a3f350bc4e842009
Aubry <ahiatt19@byu.edu>
2024-11-09 17:57:24 -0700

    rows and columns iterators are done for the chess board

--- client/src/main/java/ui/ChessBoardUI.java
+++ client/src/main/java/ui/ChessBoardUI.java
@@ -3,19 +3,18 @@ package ui;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Random;
-
-import ui.EscapeSequences.*;
+import chess.ChessBoard;
 
 import static ui.EscapeSequences.*;
 
-public class ChessBoard {
+public class ChessBoardUI {
     //Board Dimensions
     private static final int BOARD_SIZE_IN_SQUARES = 8;
     private static final int SQUARE_SIZE_IN_PADDED_CHARS = 1;
     //private static final int LINE_WIDTH_IN_PADDED_CHARS = 1;
 
     // Padded characters.
-    private static final String EMPTY = "   ";
+    private static final String EMPTY = " ";
     private static final String X = " X ";
     private static final String O = " O ";
 
@@ -24,33 +23,33 @@ public class ChessBoard {
     public static void main(String[] args) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
-        drawHeadersFooters(out);
+        drawHeaderFooter(out);
 
-        drawTicTacToeBoard(out);
+        ChessBoard chessBoard = new ChessBoard();
 
-        drawHeadersFooters(out);
+        drawChessBoard(out, chessBoard);
+
+        drawHeaderFooter(out);
 
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_WHITE);
     }
 
-    private static void drawHeadersFooters(PrintStream out) {
+    private static void drawHeaderFooter(PrintStream out) {
 
         setBlack(out);
 
+        out.print(EMPTY.repeat(3));
+
+
         String[] headers = {" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h "};
         for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-            drawHeader(out, headers[boardCol]);
-
-            //if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-            //    out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
-            //}
+            drawHeaderFooter(out, headers[boardCol]);
         }
-
         out.println();
     }
 
-    private static void drawHeader(PrintStream out, String headerText) {
+    private static void drawHeaderFooter(PrintStream out, String headerText) {
         int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
         int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
 
@@ -61,24 +60,39 @@ public class ChessBoard {
 
     private static void printHeaderText(PrintStream out, String player) {
         out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_GREEN);
+        out.print(SET_TEXT_COLOR_MAGENTA);
+        out.print(SET_TEXT_BOLD);
 
         out.print(player);
 
         setBlack(out);
     }
 
-    private static void drawTicTacToeBoard(PrintStream out) {
+    private static void printRow(PrintStream out, int row) {
+        out.print(EMPTY);
+
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_MAGENTA);
+        out.print(SET_TEXT_BOLD);
+
+        out.print(row);
+
+        setBlack(out);
+        out.print(EMPTY);
+    }
+
+    private static void drawChessBoard(PrintStream out, ChessBoard chessBoard) {
 
         for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
+            printRow(out, boardRow + 1);
 
             drawRowOfSquares(out);
-
             if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
-                // Draw horizontal row separator.
-                drawHorizontalLine(out);
+                // Draw horizontal row separator
                 setBlack(out);
             }
+            printRow(out, boardRow + 1);
+            out.println();
         }
     }
 
@@ -99,32 +113,15 @@ public class ChessBoard {
                 else {
                     out.print(EMPTY.repeat(SQUARE_SIZE_IN_PADDED_CHARS));
                 }
-
                 if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
                     // Draw vertical column separator.
                     setRed(out);
                     //out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
                 }
-
                 setBlack(out);
             }
 
-            out.println();
         }
-    }
-
-    private static void drawHorizontalLine(PrintStream out) {
-
-        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_PADDED_CHARS +
-                (BOARD_SIZE_IN_SQUARES - 1);// * LINE_WIDTH_IN_PADDED_CHARS;
-
-        //for (int lineRow = 0; lineRow < LINE_WIDTH_IN_PADDED_CHARS; ++lineRow) {
-         //   setRed(out);
-        //    out.print(EMPTY.repeat(boardSizeInSpaces));
-
-        //    setBlack(out);
-        //    out.println();
-        //}
     }
 
     private static void setWhite(PrintStream out) {
