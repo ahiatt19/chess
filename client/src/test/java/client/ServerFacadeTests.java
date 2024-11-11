1d2dd39efb50762fe6aaaab87c30fe81575c9c72
Aubry <ahiatt19@byu.edu>
2024-11-10 17:57:25 -0700

    setting up the ServerFacade class

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,24 @@
 package client;
 
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
+
+        var serverUrl = "http://localhost:8080";
+        facade = new ServerFacade(serverUrl);
     }
 
     @AfterAll
@@ -24,6 +30,13 @@ public class ServerFacadeTests {
     @Test
     public void sampleTest() {
         Assertions.assertTrue(true);
+    }
+
+    @Test
+    void register() throws Exception {
+        UserData userData = new UserData("player1", "password", "p1@email.com");
+        var authData = facade.register(userData);
+        Assertions.assertTrue(authData.authToken().length() > 10);
     }
 
 }
